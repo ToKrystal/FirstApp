0058cef864db5772b4afb81de737716305991def
ToKrystal <390654270@qq.com>
2017-05-07 23:33:15 +0800

    update jingxuan

--- app/src/main/java/com/chao/bookviki/presenter/TechPresenter.java
+++ app/src/main/java/com/chao/bookviki/presenter/TechPresenter.java
@@ -2,16 +2,17 @@ package com.chao.bookviki.presenter;
 
 import com.chao.bookviki.app.Constants;
 import com.chao.bookviki.base.RxPresenter;
+import com.chao.bookviki.component.RxBus;
 import com.chao.bookviki.model.bean.GankItemBean;
+import com.chao.bookviki.model.bean.JingXuanNewsBean;
 import com.chao.bookviki.model.event.SearchEvent;
 import com.chao.bookviki.model.http.RetrofitHelper;
 import com.chao.bookviki.model.http.response.GankHttpResponse;
-import com.chao.bookviki.util.RxUtil;
-import com.chao.bookviki.widget.CommonSubscriber;
-import com.chao.bookviki.component.RxBus;
-import com.chao.bookviki.model.bean.GankSearchItemBean;
+import com.chao.bookviki.model.http.response.JingXuanNewsResponse;
 import com.chao.bookviki.presenter.contract.TechContract;
 import com.chao.bookviki.ui.gank.fragment.GankMainFragment;
+import com.chao.bookviki.util.RxUtil;
+import com.chao.bookviki.widget.CommonSubscriber;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,9 +36,23 @@ public class TechPresenter extends RxPresenter<TechContract.View> implements Tec
     private String currentTech = GankMainFragment.tabTitle[0];
     private int currentType = Constants.TYPE_ANDROID;
 
+    /**
+     * war 	军事
+     2 	sport 	体育
+     3 	tech 	科技
+     4 	edu 	教育
+     5 	ent 	娱乐
+     6 	money 	财经
+     7 	gupiao 	股票
+     8 	travel 	旅游
+     9 	lady 	女人
+     * @param mRetrofitHelper
+     */
+
     @Inject
     public TechPresenter(RetrofitHelper mRetrofitHelper) {
         this.mRetrofitHelper = mRetrofitHelper;
+        //查询事件
         registerEvent();
     }
 
@@ -60,13 +75,40 @@ public class TechPresenter extends RxPresenter<TechContract.View> implements Tec
                     @Override
                     public void onNext(String s) {
                         queryStr = s;
-                        getSearchTechData();
+                     //   getSearchTechData();
+                        getSearchNewsDate();
                     }
                 });
         addSubscrebe(rxSubscription);
     }
 
-    private void getSearchTechData() {
+    private void getSearchNewsDate() {
+        currentPage = 1;
+        Subscription rxSubscription = mRetrofitHelper.getJingXuanNewsList("war",currentPage,NUM_OF_PAGE)
+                .compose(RxUtil.<JingXuanNewsResponse<List<JingXuanNewsBean>>>rxSchedulerHelper())
+                .compose(RxUtil.<List<JingXuanNewsBean>>handleJingXuanNewsResult())
+                .map(new Func1<List<JingXuanNewsBean>, List<JingXuanNewsBean>>() {
+                    @Override
+                    public List<JingXuanNewsBean> call(List<JingXuanNewsBean> beans) {
+                        List<JingXuanNewsBean> newList = new ArrayList<>();
+                        for (JingXuanNewsBean item : beans) {
+                            if (item.getTitle().contains(queryStr)){
+                                newList.add(item);
+                            }
+                        }
+                        return newList;
+                    }
+                })
+                .subscribe(new CommonSubscriber<List<JingXuanNewsBean>>(mView) {
+                    @Override
+                    public void onNext(List<JingXuanNewsBean> beans) {
+                        mView.showJingXuanItem(beans);
+                    }
+                });
+        addSubscrebe(rxSubscription);
+    }
+
+    /*private void getSearchTechData() {
         currentPage = 1;
         Subscription rxSubscription = mRetrofitHelper.fetchGankSearchList(queryStr, currentTech, NUM_OF_PAGE, currentPage)
                 .compose(RxUtil.<GankHttpResponse<List<GankSearchItemBean>>>rxSchedulerHelper())
@@ -94,9 +136,9 @@ public class TechPresenter extends RxPresenter<TechContract.View> implements Tec
                     }
                 });
         addSubscrebe(rxSubscription);
-    }
+    }*/
 
-    @Override
+   /* @Override
     public void getGankData(String tech, int type) {
         queryStr = null;
         currentPage = 1;
@@ -112,9 +154,48 @@ public class TechPresenter extends RxPresenter<TechContract.View> implements Tec
                     }
                 });
         addSubscrebe(rxSubscription);
-    }
+    }*/
 
     @Override
+    public void getJingXuanNews(String type) {
+        Subscription rxSubscription = mRetrofitHelper.getJingXuanNewsList(type,currentPage,NUM_OF_PAGE)
+                .compose(RxUtil.<JingXuanNewsResponse<List<JingXuanNewsBean>>>rxSchedulerHelper())
+                .compose(RxUtil.<List<JingXuanNewsBean>>handleJingXuanNewsResult())
+                .subscribe(new CommonSubscriber<List<JingXuanNewsBean>>(mView) {
+                    @Override
+                    public void onNext(List<JingXuanNewsBean> jingXuanNewsBean) {
+                        mView.showJingXuanItem(jingXuanNewsBean);
+                    }
+                });
+        addSubscrebe(rxSubscription);
+    }
+
+
+
+    @Override
+    public void getMoreJingXuanNews(String type) {
+        if(queryStr != null) {
+            //TODO 搜索的
+          //  getMoreSearchGankData();
+            getMoreSearchNewsData();
+            return;
+        }
+        Subscription rxSubscription = mRetrofitHelper.getJingXuanNewsList(type,++currentPage,NUM_OF_PAGE)
+                .compose(RxUtil.<JingXuanNewsResponse<List<JingXuanNewsBean>>>rxSchedulerHelper())
+                .compose(RxUtil.<List<JingXuanNewsBean>>handleJingXuanNewsResult())
+                .subscribe(new CommonSubscriber<List<JingXuanNewsBean>>(mView, "加载更多数据失败ヽ(≧Д≦)ノ") {
+                    @Override
+                    public void onNext(List<JingXuanNewsBean> beans) {
+                        mView.showMoreJingXuanItem(beans);
+                    }
+                });
+        addSubscrebe(rxSubscription);
+    }
+
+
+
+
+    /*@Override
     public void getMoreGankData(String tech) {
         if(queryStr != null) {
             getMoreSearchGankData();
@@ -130,9 +211,9 @@ public class TechPresenter extends RxPresenter<TechContract.View> implements Tec
                     }
                 });
         addSubscrebe(rxSubscription);
-    }
+    }*/
 
-    private void getMoreSearchGankData() {
+    /*private void getMoreSearchGankData() {
         Subscription rxSubscription = mRetrofitHelper.fetchGankSearchList(queryStr, currentTech, NUM_OF_PAGE, ++currentPage)
                 .compose(RxUtil.<GankHttpResponse<List<GankSearchItemBean>>>rxSchedulerHelper())
                 .compose(RxUtil.<List<GankSearchItemBean>>handleResult())
@@ -159,6 +240,31 @@ public class TechPresenter extends RxPresenter<TechContract.View> implements Tec
                     }
                 });
         addSubscrebe(rxSubscription);
+    }*/
+
+    private void getMoreSearchNewsData() {
+        Subscription rxSubscription = mRetrofitHelper.getJingXuanNewsList("war",currentPage,NUM_OF_PAGE)
+                .compose(RxUtil.<JingXuanNewsResponse<List<JingXuanNewsBean>>>rxSchedulerHelper())
+                .compose(RxUtil.<List<JingXuanNewsBean>>handleJingXuanNewsResult())
+                .map(new Func1<List<JingXuanNewsBean>, List<JingXuanNewsBean>>() {
+                    @Override
+                    public List<JingXuanNewsBean> call(List<JingXuanNewsBean> beans) {
+                        List<JingXuanNewsBean> newList = new ArrayList<>();
+                        for (JingXuanNewsBean item : beans) {
+                            if (item.getTitle().contains(queryStr)){
+                                newList.add(item);
+                            }
+                        }
+                        return newList;
+                    }
+                })
+                .subscribe(new CommonSubscriber<List<JingXuanNewsBean>>(mView) {
+                    @Override
+                    public void onNext(List<JingXuanNewsBean> beans) {
+                        mView.showJingXuanItem(beans);
+                    }
+                });
+        addSubscrebe(rxSubscription);
     }
 
     @Override
@@ -174,4 +280,6 @@ public class TechPresenter extends RxPresenter<TechContract.View> implements Tec
                 });
         addSubscrebe(rxSubscription);
     }
+
+
 }
